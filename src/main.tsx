41828b4f55b71ec738ad943d97ad6963bdd7b72d
Charan <ragupathicharan@gmail.com>
2025-01-25 15:32:00 +0530

    imported vercel speed analytics and insights pacakges

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,6 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <><StrictMode>
-    <App />
-  </StrictMode><Analytics /><SpeedInsights /></>
+    <App /><Analytics /><SpeedInsights />
+  </StrictMode></>
 );
